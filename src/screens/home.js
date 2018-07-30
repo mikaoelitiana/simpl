1709a4e49a5af2d422410c67765fa16e92aacc12
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2018-07-30 22:58:36 +0200

    style: minor style adjustment

--- src/screens/home.js
+++ src/screens/home.js
@@ -30,7 +30,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#F5FCFF",
+    opacity: 0.90
   },
   welcome: {
     fontSize: 20,
