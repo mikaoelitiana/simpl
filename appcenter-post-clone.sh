732d85814c68f36f64836a1356d7e5a70e6dc15c
Mika Andrianarijaona <mikaoelitiana@users.noreply.github.com>
2018-08-02 17:42:17 +0200

    display node version

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 brew uninstall node@6
-NODE_VERSION="8.9.4"
+NODE_VERSION="9.10.1"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+node -v
